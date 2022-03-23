c7e209b8a8963afcb0e0931ac5ad0556f81cf54b
Trifon Yanakiev <trifon.yanakiev@gmail.com>
2022-03-23 18:35:15 -0500

    Adding additional fields to the form format for registration.

--- AutomatedParking/members/views.py
+++ AutomatedParking/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterUserForm
 
 
 def login_user(request):
@@ -27,7 +28,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -37,5 +38,5 @@ def register_user(request):
             messages.success(request, "Registration Successful")
             return redirect('home')
     else:
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
     return render(request, 'authenticate/register_user.html', {'form':form})
