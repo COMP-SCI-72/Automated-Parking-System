62753bdf5306e262449631f16cb6598a775cb124
Creston Davison <crestonfdavison@gmail.com>
2022-04-04 14:30:56 -0500

    Start-End date functionality for reservation form

--- AutomatedParking/ParkingReservation/form.py
+++ AutomatedParking/ParkingReservation/form.py
@@ -4,14 +4,12 @@ from .models import Reservation, Car
 
 
 class ReservationForm(ModelForm):
-
     class Meta:
         model = Reservation
-        fields = ('car', 'parking')  #'__all__'
+        fields = ('car', 'parking', 'start_date', 'end_date')
 
 
 class CarForm(ModelForm):
     class Meta:
         model = Car
         fields = ('make', 'model', 'year', 'license')
-
